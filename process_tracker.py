c6a943b83509c9919ab0c101a1b6bf4e57b4bc1a
JayFromProgramming <ajsweene@mtu.edu>
2023-01-13 12:40:00 -0500

    Initial commit
    Signed-off-by: JayFromProgramming <ajsweene@mtu.edu>

---
+++ process_tracker.py
@@ -0,0 +1,56 @@
+import subprocess
+import os
+import threading
+
+
+class ProcessTracker:
+    # This class is started via multiprocessing.Process
+
+    def __init__(self, process_name, process_command, process_env_vars=None, process_cwd=None):
+        self.process_name = process_name
+        self.process_command = process_command
+        self.process_env_vars = process_env_vars
+        self.process_cwd = process_cwd
+        self.process_terminal = None
+        self.status = "Not Started"
+        self.stdout_last_line = ""
+        self.stderr_last_line = ""
+
+        # Start the process
+        self.thread = threading.Thread(target=self.start)
+        self.thread.start()
+
+    def start(self):
+        # Setup a shell with the correct environment variables (setup.bash)
+        self.process_terminal = subprocess.Popen(
+            args=self.process_command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            env=self.process_env_vars,
+            cwd=self.process_cwd
+        )
+
+        # Start the process
+        self.status = "Running"
+        while True:
+            # Read the stdout and stderr
+            stdout = self.process_terminal.stdout.readline()
+            stderr = self.process_terminal.stderr.readline()
+            if stdout:
+                self.stdout_last_line = stdout.decode("utf-8").strip()
+                print(self.stdout_last_line)
+            if stderr:
+                self.stderr_last_line = stderr.decode("utf-8").strip()
+                print(self.stderr_last_line)
+            if self.process_terminal.poll() is not None:
+                # The process has stopped
+                self.status = "Stopped"
+                break
+
+    def stop(self):
+        self.process_terminal.terminate()
+        self.status = "Stopped"
+
+
+
---
+++ start.py
@@ -0,0 +1,55 @@
+# This script starts all ros nodes and required programs for the robot
+# This includes but is not limited to:
+# - ros1_bridge
+# - roscore
+# - rosbridge_server
+# - obs (for streaming webcam)
+# - all robot nodes
+
+# import threading
+# import os
+import subprocess
+# import rich
+import process_tracker
+
+
+class Main:
+
+    def __init__(self):
+        self.roscore = None
+        self.ros1_bridge = None
+        self.rosbridge_server = None
+        self.obs = None
+        self.robot_nodes = []
+        self.threads = []
+        self.start()
+
+    def parse_env_vars(self, setup_bash_path):
+        # Execute the setup.bash file and determine which environment variables are set
+        # This is done by executing the setup.bash file and then reading the environment variables
+
+        # Execute the setup.bash file
+        process = subprocess.Popen(
+            args=f"source {setup_bash_path}",
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        process.wait()
+
+        # Read the environment variables
+        env_vars = {}
+        for line in process.stdout.readlines():
+            line = line.decode("utf-8").strip()
+            if line.startswith("export"):
+                line = line.replace("export ", "")
+                line = line.split("=")
+                env_vars[line[0]] = line[1].replace('"', "")
+        return env_vars
+
+    def start(self):
+        ros_foxy_vars = self.parse_env_vars("/opt/ros/foxy/setup.bash")
+        print(ros_foxy_vars)
+        # self.roscore = process_tracker.ProcessTracker(
+        #     process_name="roscore",
+        #     process_command="roscore",
