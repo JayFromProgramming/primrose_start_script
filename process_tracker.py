3c50242900381f98e7204eadf3a8b13f7e274d28
JayFromProgramming <ajsweene@mtu.edu>
2023-01-13 19:40:06 -0500

    Remote development
    Signed-off-by: JayFromProgramming <ajsweene@mtu.edu>

--- process_tracker.py
+++ process_tracker.py
@@ -92,7 +92,7 @@ class ProcessTracker:
                 # print(self.stderr_last_line)
             if self.process_terminal.poll() is not None:
                 # The process has stopped
-                self.status = f"Failed ({self.process_terminal.poll()})"
+                self.status = f"Execute Failed: {self.process_terminal.returncode}"
                 self.stdout_last_line = stdout.decode("utf-8").strip()
                 self.stderr_last_line = stderr.decode("utf-8").strip()
                 # print(f"Process {self.process_name} has stopped with exit code {self.process_terminal.poll()}")
