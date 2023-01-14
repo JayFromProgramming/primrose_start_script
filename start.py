a266090b04bc4408f59c7ade29cbba85951dd055
JayFromProgramming <ajsweene@mtu.edu>
2023-01-13 19:19:14 -0500

    Initial commit
    Signed-off-by: JayFromProgramming <ajsweene@mtu.edu>

--- start.py
+++ start.py
@@ -95,17 +95,17 @@ class Main:
     def display_status(self):
         # Display the status of all processes in a table
         table = Table(show_header=True, header_style="bold magenta", show_lines=True)
-        table.add_column("PID", style="dim", width=12)
-        table.add_column("Process Name")
-        table.add_column("Status")
-        table.add_column("Last Line")
+        table.add_column("PID", justify="center", style="cyan", no_wrap=True)
+        table.add_column("Process Name", width=25)
+        table.add_column("Status", width=10)
+        # table.add_column("Last Line"
         for process in self.processes:
             color = "green" if process.running else "red" if process.failed else "yellow"
             table.add_row(
                 str(process.pid),
                 process.name,
                 process.status,
-                str(process.stderr_last_line) if process.stderr_last_line else str(process.stdout_last_line),
+                # str(process.stderr_last_line) if process.stderr_last_line else str(process.stdout_last_line),
                 style=color
             )
         # Replace the previous table without clearing the console by just moving the cursor up
